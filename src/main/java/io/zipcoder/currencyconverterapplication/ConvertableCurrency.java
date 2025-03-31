f741d178f9d7d6357a7130218a2effc15799d00e
ConnorC04 <connorjcasey@iCloud.com>
2025-03-31 16:43:24 -0400

    YIPPEE

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -3,6 +3,6 @@ package io.zipcoder.currencyconverterapplication;
 public interface ConvertableCurrency {
     CurrencyType getCurrencyType();
     default Double convert(CurrencyType currencyType) {
-        return currencyType.getRate();
+        return currencyType.getRate()/getCurrencyType().getRate();
     }
 }
